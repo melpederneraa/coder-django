316250003cc30acf733d1fb9324453eaf2d25ce4
melpederneraa <melipedernera2004@gmail.com>
2025-02-17 22:34:09 -0300

    cambio para Django

--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,24 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def saludar(request):
+    return HttpResponse("Hola desde Django!")
+
+
+def saludar_con_etiqueta(request):
+    return HttpResponse("<h1>Hola desde Django con etiquetas!</h1>")
+
+
+def saludar_con_parametros(request, nombre: str, apellido: str):
+    nombre = nombre.capitalize()  # Capitaliza la primera letra del nombre
+    apellido = apellido.capitalize()  # Capitaliza la primera letra del apellido
+    return HttpResponse(f"Hola {nombre} {apellido} desde Django con parámetros!")
+
+
+def index(request):
+    from datetime import datetime
+
+    año_actual = datetime.now().year
+    contexto = {"año": año_actual}
+    return render(request, "core/index.html", contexto)
